325a2c86efd8172579e8e11b630c04bbde12369f
sunny-icpl <sunnym@infopercept.com>
2023-12-03 19:10:44 +0530

    Adding aws lambda code.

---
+++ OcsfAzureAd.Tests/FunctionTest.cs
@@ -0,0 +1,72 @@
+using Xunit;
+using Amazon.Lambda.TestUtilities;
+using Amazon.Lambda.S3Events;
+using static Amazon.Lambda.S3Events.S3Event;
+
+
+namespace OcsfAzureAd.Tests;
+
+public class FunctionTest
+{
+    [Fact]
+    public void TestToUpperFunction()
+    {
+        var evt = new S3Event
+        {
+            Records =
+            [
+                new S3EventNotificationRecord()
+                {
+                    S3 = new S3Entity()
+                    {
+                        Bucket = new S3BucketEntity()
+                        {
+                            Name = "test-bucket"
+                        },
+                        Object = new S3ObjectEntity()
+                        {
+                            Key = "test-file.txt"
+                        }
+                    }
+                }
+                ]
+        };
+
+        // Invoke the lambda function and confirm the string was upper cased.
+        var context = new TestLambdaContext();
+        var output = Function.FunctionHandler(evt, context);
+
+        Assert.Equal($"File {evt.Records[0].S3.Bucket.Name} uploaded to bucket {evt.Records[0].S3.Object.Key}{Environment.NewLine}", output.Result);
+    }
+
+    [Fact]
+    public void TestMultilineInputFunction()
+    {
+        var evt = new S3Event
+        {
+            Records =
+          [
+              new S3EventNotificationRecord()
+                {
+                    S3 = new S3Entity()
+                    {
+                        Bucket = new S3BucketEntity()
+                        {
+                            Name = "test-bucket"
+                        },
+                        Object = new S3ObjectEntity()
+                        {
+                            Key = "test-file.txt"
+                        }
+                    }
+                }
+              ]
+        };
+
+        // Invoke the lambda function and confirm the string was upper cased.
+        var context = new TestLambdaContext();
+        var output = Function.FunctionHandler(evt, context);
+
+        Assert.Equal($"File {evt.Records[0].S3.Bucket.Name} uploaded to bucket {evt.Records[0].S3.Object.Key}{Environment.NewLine}", output.Result);
+    }
+}
---
+++ OcsfAzureAd/Function.cs
@@ -0,0 +1,104 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.S3Events;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Csv;
+using System.Text;
+
+namespace OcsfAzureAd;
+
+//[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+public class Function
+{
+    private static CsvOptions csvOptions = new CsvOptions // Defaults
+    {
+        RowsToSkip = 0, // Allows skipping of initial rows without csv data
+                        //SkipRow = (row, idx) => string.IsNullOrEmpty(row) || row[0] == '#',
+        Separator = '\0', // Autodetects based on first row
+        TrimData = false, // Can be used to trim each cell
+        Comparer = null, // Can be used for case-insensitive comparison for names
+        HeaderMode = HeaderMode.HeaderPresent, // Assumes first row is a header row
+        ValidateColumnCount = false, // Checks each row immediately for column count
+        ReturnEmptyForMissingColumn = false, // Allows for accessing invalid column names
+        Aliases = null, // A collection of alternative column names
+        AllowNewLineInEnclosedFieldValues = false, // Respects new line (either \r\n or \n) characters inside field values enclosed in double quotes.
+        AllowBackSlashToEscapeQuote = false, // Allows the sequence "\"" to be a valid quoted value (in addition to the standard """")
+        AllowSingleQuoteToEncloseFieldValues = false, // Allows the single-quote character to be used to enclose field values
+        NewLine = Environment.NewLine // The new line string to use when multiline field values are read (Requires "AllowNewLineInEnclosedFieldValues" to be set to "true" for this to have any effect.)
+    };
+
+
+    /// <summary>
+    /// The main entry point for the custom runtime.
+    /// </summary>
+    /// <param name="args"></param>
+    private static async Task Main(string[] args)
+    {
+        Func<S3Event, ILambdaContext, Task<string>> handler = FunctionHandler;
+        await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
+            .Build()
+            .RunAsync();
+    }
+
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    ///
+    /// To use this handler to respond to an AWS event, reference the appropriate package from 
+    /// https://github.com/aws/aws-lambda-dotnet#events
+    /// and change the string input parameter to the desired event type.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public async static Task<string> FunctionHandler(S3Event evt, ILambdaContext context)
+    {
+        var s3Client = new AmazonS3Client();
+
+        var output = new StringBuilder();
+        try
+        {
+            var record = evt.Records[0];
+            string sourceBucket = record.S3.Bucket.Name;
+            string destinationBucket = "ocsf-sec-lake-azure"; // Set your destination bucket name here
+            string fileName = record.S3.Object.Key;
+            output.AppendLine($"File {fileName} uploaded to bucket {sourceBucket}");
+            context.Logger.LogLine($"File {fileName} uploaded to bucket {sourceBucket}");
+
+            // Read the CSV file from the source bucket
+            using (var response = await s3Client.GetObjectAsync(sourceBucket, fileName))
+            {
+                foreach (var line in CsvReader.ReadFromStream(response.ResponseStream))
+                {
+
+                }
+
+                // Write the modified records to a new CSV file
+                using (var stream = new MemoryStream())
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Flush();
+                    stream.Position = 0;
+
+                    // Upload the modified file to the destination bucket
+                    await s3Client.PutObjectAsync(new PutObjectRequest
+                    {
+                        BucketName = destinationBucket,
+                        Key = fileName,
+                        InputStream = stream
+                    });
+
+                    context.Logger.LogLine($"File processed and uploaded from {sourceBucket}/{fileName} to {destinationBucket}/{fileName}");
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            context.Logger.LogLine($"Error: {ex.Message}");
+        }
+
+        return output.ToString();
+    }
+}
